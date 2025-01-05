36dc99121e4a3bf24c3bb797ee444507965bc949
Sinantosun <sinan_tosun4141@hotmail.com>
2025-01-05 19:00:20 +0300

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,AboutComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  helloWorld: string = "hello worl form paris";
+  pClassName = "text-danger";
+
+  clickMe(){
+    alert("beni tikladın!");
+  }
+
+  changeHelloWorldVariable(event:any){
+    this.helloWorld = event.target.value;
+  }  
 }
+
+
